92c55dc60eaec47a196bcb989373e4c921108bfb
Edward Nicholes Jr <GameDevFox@gmail.com>
2022-01-14 22:12:22 -0600

    Replace new line with EOL constant

--- core/main.c
+++ core/main.c
@@ -30,7 +30,7 @@ void printAddress(size_t* addr,  void (*out)(unsigned char)) {
 }
 
 void printCycleCounterList() {
-	uart_puts("CYCLE COUNTER LIST:\r\n");
+	uart_puts("CYCLE COUNTER LIST:" EOL);
 
 	size_t prev = 0;
 	for(int i=0; i<8; i++) {
@@ -46,12 +46,12 @@ void printCycleCounterList() {
 			wordToHex(delta, uart_putc);
 		}
 
-		uart_puts("\r\n");
+		uart_puts(EOL);
 	}
 }
 
 void walkMemory(size_t* start, void (*out)(unsigned char)) {
-	uart_puts("== MEMORY WALK ==\r\n");
+	uart_puts("== MEMORY WALK ==" EOL);
 
 	size_t* current = start;
 
@@ -75,11 +75,11 @@ void walkMemory(size_t* start, void (*out)(unsigned char)) {
 			increment = 1;
 		} else {
 			increment = hex;
-			uart_puts("--------\r\n");
+			uart_puts("--------" EOL);
 		}
 	}
 
-	uart_puts("== WALK FINISHED ==\r\n");
+	uart_puts("== WALK FINISHED ==" EOL);
 }
 
 void readMemory() {
@@ -108,7 +108,7 @@ void writeMemory() {
 		*(char*)i = value;
 	}
 
-	uart_puts(" Done!\r\n");
+	uart_puts(" Done!" EOL);
 }
 
 int runLoop = 1;
@@ -124,21 +124,21 @@ void backspace() {
 void printTimer() {
 	uint32_t time = readTimer();
 	wordToHex(time, uart_putc);
-	uart_puts("\r\n");
+	uart_puts(EOL);
 }
 
 void generateMRC(bool isMRC) {
-  uart_puts("coproc\r\n");
+  uart_puts("coproc" EOL);
   char coproc = getb();
-  uart_puts("opc1\r\n");
+  uart_puts("opc1" EOL);
   char opc1 = getb();
-  // uart_puts("Rt\r\n");
+  // uart_puts("Rt" EOL);
   // char Rt = getb();
-  uart_puts("CRn\r\n");
+  uart_puts("CRn" EOL);
   char CRn = getb();
-  uart_puts("CRm\r\n");
+  uart_puts("CRm" EOL);
   char CRm = getb();
-  uart_puts("opc2\r\n");
+  uart_puts("opc2" EOL);
   char opc2 = getb();
 
   char Rt = 0;
@@ -147,13 +147,13 @@ void generateMRC(bool isMRC) {
     coproc, opc1, Rt, CRn, CRm, opc2, flags
   );
   wordToHex(binaryEntry, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void writeWord() {
   uart_puts("Enter hex to write to ");
   wordToHex(binaryEntry, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 
   uint32_t value = 0;
   value |= getb() << 0x1c;
@@ -169,33 +169,33 @@ void writeWord() {
 
   uart_puts("Wrote ");
   wordToHex(value, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void initIRQ() {
-	uart_puts("enableIRQ and routeIRQtoCPUS\r\n");
+	uart_puts("enableIRQ and routeIRQtoCPUS" EOL);
 	enableIRQ(VC_IRQ_TIMER_1);
 	routeIRQtoCPUS(VC_IRQ_TIMER_1, 0x01);
-	uart_puts("Done\r\n");
+	uart_puts("Done" EOL);
 }
 
 void in3Seconds() {
-  uart_puts("Set time 3 seconds from now\r\n");
+  uart_puts("Set time 3 seconds from now" EOL);
   uint32_t t = readTimer();
   writeTimerCompare(1, t + (3 * 1000000));
-  uart_puts("Done\r\n");
+  uart_puts("Done" EOL);
 }
 
 void printCPSR() {
   size_t mode = getCPSR();
   wordToHex(mode, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void performanceTest() {
   perfTest();
   printCycleCounterList();
-  uart_puts("Done\r\n" "\r\n");
+  uart_puts("Done" EOL EOL);
 }
 
 void callInstruction() {
@@ -205,35 +205,35 @@ void callInstruction() {
   // Call function and print result
   size_t result = liveFn();
   wordToHex(result, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void doHalt() {
   runLoop = 0;
-  uart_puts("HALT\r\n");
+  uart_puts("HALT" EOL);
 }
 
 void disablePolling() {
-  uart_puts("Disabled...\r\n");
+  uart_puts("Disabled..." EOL);
   isPollingEnabled = 0;
 }
 
 void doToggleIRQ() {
   size_t intMode = toggleIRQs();
   wordToHex(intMode, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void doSetMode() {
   setMode(binaryEntry);
   binaryEntry = getCPSR();
   wordToHex(binaryEntry, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void doEnableCache() {
   enable_cache();
-  uart_puts("Cache enabled\r\n");
+  uart_puts("Cache enabled" EOL);
 }
 
 void printCursorPos() {
@@ -243,7 +243,7 @@ void printCursorPos() {
   wordToHex(pos.row, uart_putc);
   uart_puts(" Column: ");
   wordToHex(pos.column, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void printVTSize() {
@@ -253,7 +253,7 @@ void printVTSize() {
   wordToHex(size.row, uart_putc);
   uart_puts(" Columns: ");
   wordToHex(size.column, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 void printPerformanceCounter() {
@@ -350,7 +350,7 @@ void runCounter() {
 
   uart_puts(VT_SAVE);
 
-  VT_setPos(0, 0);
+  VT_setPos(1, 2);
   uart_puts(VT_BG_RED VT_BLACK);
   write_digits(counter, uart_putc);
   uart_puts(VT_DEFAULT VT_BG_DEFAULT);
@@ -358,14 +358,134 @@ void runCounter() {
   uart_puts(VT_LOAD);
 }
 
+#define ENTER 0x0d
+#define ESCAPE 0x1b
+#define BACKSPACE 0x7f
+
+void mainHandler(char);
+void setInputHandler(void (*handler)(char));
+
+void (*inputHandler)(char) = mainHandler;
+
+void rawHandler(char c) {
+  byteToHex(c, uart_putc);
+  uart_putc(' ');
+}
+
+void commandHandler(char input) {
+  switch(input) {
+    case '`': generateMRC(1); break;
+    case '~': generateMRC(0); break;
+
+    case '1': uart_puts("Hello, Kitsune!" EOL); break;
+    case '2': asm(SWI(1234)); break;
+    case '3': printCPSR(); break;
+    case '4': performanceTest(); break;
+    case '5': asm(".word 0xffffffff"); break;
+    case '6': fbTest(); break;
+    case '7': fbTest2(); break;
+    case '9': drawLogo(); break;
+    case '0': fbClear(); break;
+
+    case '!': callInstruction(); break;
+    case '@': disablePolling(); break;
+    case '=': binaryEntryMode(); break;
+
+    case 'q': doToggleIRQ(); break;
+    case 'w': writeWord(); break;
+    case 'r': walkMemory((size_t*)binaryEntry, uart_putc); break;
+    case 't': printTimer(); break;
+
+    case 'a': uart_puts(VT_SAVE VT_HOME VT_RED "Red Text" EOL VT_DEFAULT VT_LOAD); break;
+    case 's': doEnableCache(); break;
+    case 'd': printCursorPos(); break;
+    case 'f': printVTSize(); break;
+    case 'h': VT_fill('X'); break;
+    case 'j': setInputHandler(rawHandler); break;
+    case 'k': printPerformanceCounter(); break;
+    case 'l': VT_fill(' '); break;
+
+    case 'z': initIRQ(); break;
+    case 'x': writeTimerCompare(1, binaryEntry); break;
+    case 'c': in3Seconds(); break;
+    case 'b': doHalt(); break;
+    case 'm': doSetMode(); break;
+
+    case 'W': writeMemory(); break;
+    case 'R': readMemory(); break;
+
+    default:
+      uart_putc(input);
+      break;
+  }
+}
+
+void singleCommandHandler(char c) {
+  setInputHandler(mainHandler);
+  commandHandler(c);
+}
+
+void mainHandler(char c) {
+  switch(c) {
+    case '`': setInputHandler(singleCommandHandler); break;
+    case '~': setInputHandler(commandHandler); break;
+
+    case ENTER: uart_puts(EOL); break;
+    case BACKSPACE: backspace(); break;
+
+    default:
+      uart_putc(c);
+      break;
+  }
+}
+
+void rootHandler(char c) {
+  if(c == ESCAPE)
+    setInputHandler(mainHandler);
+  else
+    inputHandler(c);
+}
+
+void setInputHandler(void (*handler)(char)) {
+  char* label;
+
+  char* color = VT_WHITE;
+  char* bgColor = VT_BG_RED;
+
+  if(handler == mainHandler) {
+    label = "Normal Mode";
+    color = VT_BLACK;
+    bgColor = VT_BG_GREEN;
+  }
+  if(handler == singleCommandHandler)
+    label = "Single Command Mode";
+  if(handler == commandHandler)
+    label = "Command Mode";
+  if(handler == rawHandler)
+    label = "Raw Mode";
+
+  uart_puts(VT_SAVE);
+
+  // struct VT_Size size = VT_getSize();
+  VT_setPos(1, 1);
+
+  uart_puts(color);
+  uart_puts(bgColor);
+  uart_puts(" == ");
+  uart_puts(label);
+  uart_puts(" == " EOL VT_DEFAULT VT_BG_DEFAULT);
+
+  uart_puts(VT_LOAD);
+
+  inputHandler = handler;
+}
+
 void processInput() {
   if(isPollingEnabled)
-      uart_getc_pipe(charHandler);
+    uart_getc_pipe(rootHandler);
 }
 
 void input_loop() {
-  charHandler = input_switch;
-
   while (runLoop) {
     // List "processes" here
     runCounter();
@@ -378,28 +498,26 @@ void memoryRangeOut(size_t* start, size_t count, void (*out)(unsigned char)) {
 
   for(size_t* i = start; i < end; i++) {
     printAddress(i, out);
-    out('\r');
-    out('\n');
+    uart_puts(EOL);
   }
 
-  out('\r');
-  out('\n');
+  uart_puts(EOL);
 }
 
 void printParams(uint32_t r0, uint32_t r1, uint32_t atags) {
   uart_puts("r0:    0x");
   wordToHex(r0, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 
   uart_puts("r1:    0x");
   wordToHex(r1, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 
   uart_puts("atags: 0x");
   wordToHex(atags, uart_putc);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 
-  uart_puts("\r\n");
+  uart_puts(EOL);
 }
 
 #if defined(__cplusplus)
@@ -418,16 +536,16 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
   uart_init();
 
   uart_puts("Hello, Kitsune!" EOL);
-  uart_puts("\r\n");
+  uart_puts(EOL);
 
   printParams(r0, r1, atags);
 
-  // uart_puts("DEVICE TREE:\r\n");
+  // uart_puts("DEVICE TREE:" EOL);
   // memoryRangeOut((size_t*)atags, 32, uart_putc);
 
   drawLogo();
 
-  uart_puts("READY\r\n");
+  uart_puts("READY" EOL);
 
   input_loop();
 }
--- core/uart.c
+++ core/uart.c
@@ -10,97 +10,97 @@ static uint32_t MMIO_BASE;
 
 // The MMIO area base address, depends on board type
 static void mmio_init() {
-	switch (raspi) {
-			case 2:
-			case 3:  MMIO_BASE = 0x3f000000; break; // for raspi2 & 3
-			case 4:  MMIO_BASE = 0xfe000000; break; // for raspi4
-			default: MMIO_BASE = 0x20000000; break; // for raspi1, raspi zero etc.
-	}
+  switch (raspi) {
+      case 2:
+      case 3:  MMIO_BASE = 0x3f000000; break; // for raspi2 & 3
+      case 4:  MMIO_BASE = 0xfe000000; break; // for raspi4
+      default: MMIO_BASE = 0x20000000; break; // for raspi1, raspi zero etc.
+  }
 }
 
 // Memory-Mapped I/O output
 static void mmio_write(uint32_t reg, uint32_t data) {
-	write(MMIO_BASE + reg, data);
+  write(MMIO_BASE + reg, data);
 }
 
 // Memory-Mapped I/O input
 static uint32_t mmio_read(uint32_t reg) {
-	return read(MMIO_BASE + reg);
+  return read(MMIO_BASE + reg);
 }
 
 #define ONE "#1"
 
 // Loop <delay> times in a way that the compiler won't optimize away
 static void delay(uint32_t count) {
-	asm volatile(
-		"__delay_%=: subs %[count], %[count], " ONE "; bne __delay_%=\n"
-		 : "=r"(count): [count]"0"(count) : "cc"
-	);
+  asm volatile(
+    "__delay_%=: subs %[count], %[count], " ONE "; bne __delay_%=\n"
+      : "=r"(count): [count]"0"(count) : "cc"
+  );
 }
 
 // A Mailbox message with set clock rate of PL011 to 3MHz tag
 volatile unsigned int  __attribute__((aligned(16))) mbox[9] = {
-    9*4, 0, 0x38002, 12, 8, 2, 3000000, 0 ,0
+  9*4, 0, 0x38002, 12, 8, 2, 3000000, 0 ,0
 };
 
 void uart_init() {
-	mmio_init();
+  mmio_init();
 
-	// Disable UART0.
-	mmio_write(UART0_CR, 0x00000000);
-	// Setup the GPIO pin 14 && 15.
+  // Disable UART0.
+  mmio_write(UART0_CR, 0x00000000);
+  // Setup the GPIO pin 14 && 15.
 
-	// Disable pull up/down for all GPIO pins & delay for 150 cycles.
-	mmio_write(GPPUD, 0x00000000);
-	delay(150);
+  // Disable pull up/down for all GPIO pins & delay for 150 cycles.
+  mmio_write(GPPUD, 0x00000000);
+  delay(150);
 
-	// Disable pull up/down for pin 14,15 & delay for 150 cycles.
-	mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
-	delay(150);
+  // Disable pull up/down for pin 14,15 & delay for 150 cycles.
+  mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+  delay(150);
 
-	// Write 0 to GPPUDCLK0 to make it take effect.
-	mmio_write(GPPUDCLK0, 0x00000000);
+  // Write 0 to GPPUDCLK0 to make it take effect.
+  mmio_write(GPPUDCLK0, 0x00000000);
 
-	// Clear pending interrupts.
-	mmio_write(UART0_ICR, 0x7FF);
+  // Clear pending interrupts.
+  mmio_write(UART0_ICR, 0x7FF);
 
-	// Set integer & fractional part of baud rate.
-	// Divider = UART_CLOCK/(16 * Baud)
-	// Fraction part register = (Fractional part * 64) + 0.5
-	// Baud = 115200.
+  // Set integer & fractional part of baud rate.
+  // Divider = UART_CLOCK/(16 * Baud)
+  // Fraction part register = (Fractional part * 64) + 0.5
+  // Baud = 115200.
 
-	// For Raspi3 and 4 the UART_CLOCK is system-clock dependent by default.
-	// Set it to 3Mhz so that we can consistently set the baud rate
-	if (raspi >= 3) {
-		// UART_CLOCK = 30000000;
-		unsigned int r = (((unsigned int)(&mbox) & ~0xF) | 8);
-		// wait until we can talk to the VC
-		while ( mmio_read(MBOX_STATUS) & 0x80000000 ) { }
-		// send our message to property channel and wait for the response
-		mmio_write(MBOX_WRITE, r);
-		while ( (mmio_read(MBOX_STATUS) & 0x40000000) || mmio_read(MBOX_READ) != r ) { }
-	}
+  // For Raspi3 and 4 the UART_CLOCK is system-clock dependent by default.
+  // Set it to 3Mhz so that we can consistently set the baud rate
+  if (raspi >= 3) {
+    // UART_CLOCK = 30000000;
+    unsigned int r = (((unsigned int)(&mbox) & ~0xF) | 8);
+    // wait until we can talk to the VC
+    while ( mmio_read(MBOX_STATUS) & 0x80000000 ) { }
+    // send our message to property channel and wait for the response
+    mmio_write(MBOX_WRITE, r);
+    while ( (mmio_read(MBOX_STATUS) & 0x40000000) || mmio_read(MBOX_READ) != r ) { }
+  }
 
-	// Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
-	mmio_write(UART0_IBRD, 1);
-	// Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
-	mmio_write(UART0_FBRD, 40);
+  // Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
+  mmio_write(UART0_IBRD, 1);
+  // Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
+  mmio_write(UART0_FBRD, 40);
 
-	// Enable FIFO & 8 bit data transmission (1 stop bit, no parity).
-	mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+  // Enable FIFO & 8 bit data transmission (1 stop bit, no parity).
+  mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
 
-	// Mask all interrupts.
-	mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
-	                       (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
+  // Mask all interrupts.
+  mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
+                          (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
 
-	// Enable UART0, receive & transfer part of UART.
-	mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+  // Enable UART0, receive & transfer part of UART.
+  mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
 }
 
 void uart_putc(unsigned char c) {
-	// Wait for UART to become ready to transmit.
-	while ( mmio_read(UART0_FR) & (1 << 5) ) { }
-	mmio_write(UART0_DR, c);
+  // Wait for UART to become ready to transmit.
+  while ( mmio_read(UART0_FR) & (1 << 5) ) { }
+  mmio_write(UART0_DR, c);
 }
 
 unsigned char uart_getc(void) {
@@ -110,15 +110,15 @@ unsigned char uart_getc(void) {
 }
 
 void uart_getc_pipe(void out()) {
-	bool charAvailable = !(mmio_read(UART0_FR) & (1 << 4));
+  bool charAvailable = !(mmio_read(UART0_FR) & (1 << 4));
 
-	if(charAvailable) {
-		char c = mmio_read(UART0_DR);
-		out(c);
-	}
+  if(charAvailable) {
+    char c = mmio_read(UART0_DR);
+    out(c);
+  }
 }
 
 void uart_puts(const char* str) {
-	for (uint32_t i = 0; str[i] != '\0'; i++)
-		uart_putc((unsigned char)str[i]);
+  for (uint32_t i = 0; str[i] != '\0'; i++)
+    uart_putc((unsigned char)str[i]);
 }
